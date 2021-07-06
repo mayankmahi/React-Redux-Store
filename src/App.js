8af4456f09f9ba057fc8ed043c3a4a88c000c195
mayankmahi <mkmahi1198@gmail.com>
2021-07-06 18:42:20 +0530

    React-Redux-App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductList from "./containers/ProductList";
+import ProductDetails from "./containers/ProductDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route exact path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
